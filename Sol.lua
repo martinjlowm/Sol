6b57863a8a56fa100916ed64ebcddc5ccffbb084
Martin Jesper Low Madsen <martin@martinjlowm.dk>
2018-03-06 23:30:01 +0100

    Updates copyright notice

--- Sol.lua
+++ Sol.lua
@@ -1,21 +1,6 @@
 --[[
-    Name: Sol
-    Revision: $Rev:  $
-    $Date:  $
-    Author(s): martinjlowm (martin@martinjlowm.dk)
-    Git:
 
-    Description:
-
-    Sol reimplements common Lua functions that are only available in Lua
-    versions post 5.0. Sol means ``Sun'' in Portuguese, thus this library
-    attempts to balance out any shortcomings in the present Lua environment of
-    World of Warcraft 1.12.
-
-
-    Copyright (C) 2016 Martin Jesper Low Madsen
-
-    License:
+    Copyright (c) 2018 Martin Jesper Low Madsen
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published by the
@@ -30,6 +15,7 @@
     You should have received a copy of the GNU Lesser General Public License
     along with this library; if not, write to the Free Software Foundation,
     Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
 --]]
 
 
